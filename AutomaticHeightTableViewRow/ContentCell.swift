7b5474f8f89bb028819342a17c4de896d6c15aeb
George Malayil Philip <george.malayil@roguemonkey.in>
2022-01-17 23:52:44 +0530

    first commit

---
+++ AutomaticHeightTableViewRow/ContentCell.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  AutomaticHeightTableViewRow
+//
+//  Created by George Philip Malayil on 17/01/22.
+//
+
+import Foundation
---
+++ AutomaticHeightTableViewRow/PreviewsView.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewsView.swift
+//  AutomaticHeightTableViewRow
+//
+//  Created by George Philip Malayil on 17/01/22.
+//
+
+import Foundation
